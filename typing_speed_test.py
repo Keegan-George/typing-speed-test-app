2da2e08385fe8d3a488b495ebedf79b23c1e4765
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-16 01:33:03 -0400

    Rename functions for clarity

--- typing_speed_test.py
+++ typing_speed_test.py
@@ -14,7 +14,7 @@ class TypingSpeedTest:
 
         # key bindings
         self.window.bind("<Key>", self.timer.start)
-        self.window.bind("<space>", self.update_status_and_get_next_word)
+        self.window.bind("<space>", self.update_score_and_get_next_word)
 
         self.correct_words = 0
 
@@ -26,7 +26,7 @@ class TypingSpeedTest:
         self.ui.display_word(self.word_bank.get_random_word())
         self.ui.clear_input()
 
-    def update_status(self, _event=None):
+    def update_score(self, _event=None):
         if self.word_bank.get_current_word() == self.ui.get_input_text():
             self.correct_words += 1
             self.ui.display_success()
@@ -36,6 +36,6 @@ class TypingSpeedTest:
 
         self.ui.update_wpm(self.correct_words, self.timer.elapsed_time)
 
-    def update_status_and_get_next_word(self, _event=None):
-        self.update_status()
+    def update_score_and_get_next_word(self, _event=None):
+        self.update_score()
         self.next_word()
